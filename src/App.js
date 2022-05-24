000e82e7a0a7366591437d362b4c77d88319b06c
Klaudia Stanczyk <klaudia.stanczyk92@gmail.com>
2022-05-24 19:14:00 +0200

    xx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
+import { useState } from "react";
+import Timer from "./components/Timer";
+
 const App = () => {
+
+  const [time, setTime] = useState(0);
+  const [timerId, updateTimerId] = useState(null);
+
+  const update = () => {
+    setTime(time => time + 1)
+  }
+
+  const startTime = () => {
+    updateTimerId(setInterval(update, 1000))
+
+  }
+
+  const stopTime = () => {
+    clearInterval(timerId);
+    updateTimerId(null);
+  }
+
+
+
+
   return (
-    <h1>Hello world</h1>
+    <div>
+    <div> {{time}}></div>
+    <div>
+      <button onClick={startTime}>Start</button>
+      <button onClick={stopTime}>STOP</button>
+    </div>
+    </div>
   );
 };
 
---
+++ src/components/Timer.js
@@ -0,0 +1,19 @@
+
+
+const Timer = props => {
+
+  const start = () => {
+
+  }
+
+  return (
+    <div>
+    <div>tu bedzie czas</div>
+      <button onClick={startTime}>Start</button>
+      <button onClick={stopTime}>STOP</button>
+      <button onClick={resetTime}>RESET</button>
+    </div>
+  )
+}
+
+export default Timer;
